61113a735a1c18e074cc5529c1bd16e0823431f4
Navyrine <wahidadinugrohoo@gmail.com>
2025-03-31 22:24:12 +0800

    Membuat hive generator

---
+++ schedular_app_flutter/lib/feature/auth/domain/entities/user_entity.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class UserEntityAdapter extends TypeAdapter<UserEntity> {
+  @override
+  final int typeId = 0;
+
+  @override
+  UserEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return UserEntity(
+      username: fields[1] as String,
+      email: fields[2] as String,
+      password: fields[3] as String,
+      token: fields[4] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, UserEntity obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(1)
+      ..write(obj.username)
+      ..writeByte(2)
+      ..write(obj.email)
+      ..writeByte(3)
+      ..write(obj.password)
+      ..writeByte(4)
+      ..write(obj.token);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UserEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
